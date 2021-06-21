9a6cbb5918d623893d85360a071fd1c7391ff075
alexk77777 <misas777@gmail.com>
2021-06-21 14:32:17 +0900

    로그인 메시지

--- lib/ui/login/login_page.dart
+++ lib/ui/login/login_page.dart
@@ -7,7 +7,9 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Center(
+        child: RaisedButton(onPressed: () {  }, child: Text('Google Login'),color: Colors.red,),
+      )
     );
   }
 }
